f61aa7539669b8acbab4e8d65e7ad18802476f52
Eric Yan <eyan9@gatech.edu>
2018-04-26 15:26:07 -0400

    client changed

--- client.py
+++ client.py
@@ -35,36 +35,38 @@ while True:
     #receive server's packet
     receive_packet = sock.recv(1024)
     if receive_packet:
-        m = struct.unpack_from(b'b',receive_packet)
-        #if it is only message packet
-        if m[0] != 0:
-            m = struct.unpack_from(b'b'+str(m[0])+'s',receive_packet)
-            print m[1]
-            # if it is invalid guess
-            if 'Error' in m[1]:
-                print 'Letter to guess: ',
-                client_guess = raw_input()
-                send_packet = struct.pack('b'+str(len(client_guess))+'s', len(client_guess), client_guess)
-                sock.send(send_packet)
-            # if the game is over
-            elif 'Over' in m[1]:
-                break
-        else:
-            # unpack the client's packet
-            m = struct.unpack_from(b'bbb', receive_packet)
-            word_length = m[1]
-            num_incorrect = m[2]
-            m = struct.unpack_from(b'bbb'+str(word_length + num_incorrect)+'s', receive_packet)
-            return_string = m[3]
-            print return_string[:word_length]
-            print "Incorrect Guesses: " + return_string[word_length:]
-            print
-            print 'Letter to guess: ',
-
-            # input guess word and send the client packet to the server
-            client_guess = raw_input()
-            send_packet = struct.pack('b'+str(len(client_guess))+'s', len(client_guess), client_guess)
-            sock.send(send_packet)
+        ## m = struct.unpack_from(b'b',receive_packet)
+        ## #if it is only message packet
+        ## if m[0] != 0:
+        ##     m = struct.unpack_from(b'b'+str(m[0])+'s',receive_packet)
+        ##     print m[1]
+        ##     # if it is invalid guess
+        ##     if 'Error' in m[1]:
+        ##         print 'Letter to guess: ',
+        ##         client_guess = raw_input()
+        ##         send_packet = struct.pack('b'+str(len(client_guess))+'s', len(client_guess), client_guess)
+        ##         sock.send(send_packet)
+        ##     # if the game is over
+        ##     elif 'Over' in m[1]:
+        ##         break
+        ## else:
+        ##     # unpack the client's packet
+        ##     m = struct.unpack_from(b'bbb', receive_packet)
+        ##     word_length = m[1]
+        ##     num_incorrect = m[2]
+        ##     m = struct.unpack_from(b'bbb'+str(word_length + num_incorrect)+'s', receive_packet)
+        ##     return_string = m[3]
+        ##     print return_string[:word_length]
+        ##     print "Incorrect Guesses: " + return_string[word_length:]
+        ##     print
+        ##     print 'Letter to guess: ',
+        ## 
+        ##     # input guess word and send the client packet to the server
+        ##     client_guess = raw_input()
+        ##     send_packet = struct.pack('b'+str(len(client_guess))+'s', len(client_guess), client_guess)
+            print(receive_packet)
+            message = raw_input()
+            sock.send(message)
 
 # Finished Game
 sock.close
